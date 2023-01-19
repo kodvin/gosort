98196b80ed29cd93b42d7048cffb8dfa95bc70ff
Andrius Paulauskas <andrius.paulauskas@teliacompany.com>
2023-01-20 00:08:19 +0200

    add tests for floats and add missing string test

--- pkg/sort/sort_test.go
+++ pkg/sort/sort_test.go
@@ -15,23 +15,17 @@ var expectedAscendingString []string = []string{"animals", "cat", "dog", "mice",
 var expectedDescendingString []string = []string{"other", "mice", "dog", "cat", "animals"}
 var initialString []string = []string{"dog", "cat", "mice", "other", "animals"}
 
+var expectedAscendingFloat []float32 = []float32{1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 10.1, 100.1, 101.1, 150.1, 150.1, 200.1}
+var expectedDescendingFloat []float32 = []float32{200.1, 150.1, 150.1, 101.1, 100.1, 10.1, 6.1, 5.1, 4.1, 3.1, 2.1, 1.1}
+var initialFloat []float32 = []float32{1.1, 101.1, 3.1, 150.1, 4.1, 6.1, 10.1, 2.1, 100.1, 150.1, 200.1, 5.1}
+
 func TestNumbers_DefaultSorting(t *testing.T) {
 	unordered := make([]int, len(initial))
 
 	for _, sType := range AllSortTypes {
 		copy(unordered, initial)
 		Sort(sType, unordered, nil)
-		assert.Equal(t, expectedAscending, unordered, "SortType: "+sType)
-	}
-}
-
-func TestString_DefaultSorting(t *testing.T) {
-	unordered := make([]string, len(initialString))
-
-	for _, sType := range AllSortTypes {
-		copy(unordered, initialString)
-		Sort(sType, unordered, nil)
-		assert.Equal(t, expectedAscendingString, unordered, "SortType: "+sType)
+		assert.Equal(t, expectedAscending, unordered, "Sorting int. SortType: "+sType)
 	}
 }
 
@@ -40,7 +34,7 @@ func TestNumbers_CustomSorting_Ascending(t *testing.T) {
 	for _, sType := range AllSortTypes {
 		copy(unordered, initial)
 		Sort(sType, unordered, &common.Options[int]{Fn: func(a, b int) bool { return (a > b) }})
-		assert.Equal(t, expectedAscending, unordered, "SortType: "+sType)
+		assert.Equal(t, expectedAscending, unordered, "Sorting int. SortType: "+sType)
 	}
 }
 
@@ -50,16 +44,60 @@ func TestNumbers_CustomSorting_Descending(t *testing.T) {
 	for _, sType := range AllSortTypes {
 		copy(unordered, initial)
 		Sort(sType, unordered, &common.Options[int]{Fn: func(a, b int) bool { return (a < b) }})
-		assert.Equal(t, expectedDescending, unordered, "SortType: "+sType)
+		assert.Equal(t, expectedDescending, unordered, "Sorting int. SortType: "+sType)
 	}
 }
 
+func TestString_DefaultSorting(t *testing.T) {
+	unordered := make([]string, len(initialString))
+
+	for _, sType := range AllSortTypes {
+		copy(unordered, initialString)
+		Sort(sType, unordered, nil)
+		assert.Equal(t, expectedAscendingString, unordered, "Sorting string. SortType: "+sType)
+	}
+}
+func TestString_CustomSorting_Ascending(t *testing.T) {
+	unordered := make([]string, len(initialString))
+	for _, sType := range AllSortTypes {
+		copy(unordered, initialString)
+		Sort(sType, unordered, &common.Options[string]{Fn: func(a, b string) bool { return (a > b) }})
+		assert.Equal(t, expectedAscendingString, unordered, "Sorting string. SortType: "+sType)
+	}
+}
 func TestString_CustomSorting_Descending(t *testing.T) {
 	unordered := make([]string, len(initialString))
 
 	for _, sType := range AllSortTypes {
 		copy(unordered, initialString)
 		Sort(sType, unordered, &common.Options[string]{Fn: func(a, b string) bool { return (a < b) }})
-		assert.Equal(t, expectedDescendingString, unordered, "SortType: "+sType)
+		assert.Equal(t, expectedDescendingString, unordered, "Sorting string. SortType: "+sType)
+	}
+}
+
+func TestFloat_DefaultSorting(t *testing.T) {
+	unordered := make([]float32, len(initialFloat))
+
+	for _, sType := range AllSortTypes {
+		copy(unordered, initialFloat)
+		Sort(sType, unordered, nil)
+		assert.Equal(t, expectedAscendingFloat, unordered, "Sorting string. SortType: "+sType)
+	}
+}
+func TestFloat_CustomSorting_Ascending(t *testing.T) {
+	unordered := make([]float32, len(initialFloat))
+	for _, sType := range AllSortTypes {
+		copy(unordered, initialFloat)
+		Sort(sType, unordered, &common.Options[float32]{Fn: func(a, b float32) bool { return (a > b) }})
+		assert.Equal(t, expectedAscendingFloat, unordered, "Sorting string. SortType: "+sType)
+	}
+}
+func TestFloat_CustomSorting_Descending(t *testing.T) {
+	unordered := make([]float32, len(initialFloat))
+
+	for _, sType := range AllSortTypes {
+		copy(unordered, initialFloat)
+		Sort(sType, unordered, &common.Options[float32]{Fn: func(a, b float32) bool { return (a < b) }})
+		assert.Equal(t, expectedDescendingFloat, unordered, "Sorting string. SortType: "+sType)
 	}
 }
